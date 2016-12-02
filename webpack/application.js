703e5e0fc99e14e0843909a7db919fbc0e0f1662
Peter Krutz <emptywalls@gmail.com>
2016-12-01 21:57:52 -0800

    mounter update

--- webpack/application.js
+++ webpack/application.js
@@ -10,25 +10,38 @@ const RailsViewComponents = { Piano }
 const dynamicInjectionElements = document.querySelectorAll('[data-inject-react-class]')
 
 // Only iterate if there's any element
-if (dynamicInjectionElements !== null)
-  dynamicInjectionElements.forEach(element => {
-    const componentName = element.getAttribute('data-inject-react-class')
-    const props = JSON.parse(element.getAttribute('data-react-props'))
+if (typeof dynamicInjectionElements === "object" && dynamicInjectionElements.length > 0) {
+  for (let i=0; i < dynamicInjectionElements.length; i++ ) {
+    const element = dynamicInjectionElements[i]
+    // THIS retarded comparison is only here because phantomjs screws up the array from querySelectorAll
+    // by doing one of the following:
+    //
+    // 1. return [undefined]
+    // 2. return undefined
+    // 3. returns [object NodeList]
+    //
+    // It does this all the time and mixes valid node objects with invalid ones (undefined mainly)
+    // https://github.com/ariya/phantomjs/issues/13161 - this was reported in April 2015 and it's still a bug
+    if (element) {
+      const componentName = element.getAttribute('data-inject-react-class')
+      const props = JSON.parse(element.getAttribute('data-react-props'))
 
-    const ReactComponent = ((str) => {
-      if (typeof str !== 'string')
-        throw new TypeError('must pass in type of string.')
+      const ReactComponent = ((str) => {
+        if (typeof str !== 'string')
+          throw new TypeError('must pass in type of string.')
 
-      if (str.match(/\W|\d/))
-        throw new SyntaxError(`${str}: must pass in a valid Javascript name.`)
+        if (str.match(/\W|\d/))
+          throw new SyntaxError(`${str}: must pass in a valid Javascript name.`)
 
-      const component = RailsViewComponents[str]
+        const component = RailsViewComponents[str]
 
-      if (!component)
-        throw `Invalid component: \`${str}\` does not exist.`
+        if (!component)
+          throw `Invalid component: \`${str}\` does not exist.`
 
-      return component
-    })(componentName)
+        return component
+      })(componentName)
 
-    render(<ReactComponent {...props} />, element)
-  })
+      render(<ReactComponent {...props} />, element)
+    }
+  }
+}
