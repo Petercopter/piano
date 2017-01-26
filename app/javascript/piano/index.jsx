4e762bd8bcd12125e92a6adfb32129166b02ec1f
Peter Krutz <emptywalls@gmail.com>
2017-01-25 21:16:27 -0800

    changes

--- app/javascript/piano/index.jsx
+++ app/javascript/piano/index.jsx
@@ -111,6 +111,6 @@ export default class Piano extends React.Component {
 document.addEventListener('DOMContentLoaded', event => {
   ReactDOM.render(
     <Piano />,
-    document.body.appendChild(document.createElement('div'))
+    document.getElementById('piano-root')
   )
 })
